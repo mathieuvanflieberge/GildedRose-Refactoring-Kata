d26006b69c5deabc9ac27243a5a4d90e45ee64c4
mathieuvanflieberge <mathieu.van.flieberge@hotmail.com>
2025-02-16 16:28:11 +0100

    refactor gilded rose kata

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,14 +1,16 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+import { Item } from "./classes/item";
+import {
+  updateQualityBasedOnSellIn,
+  updateQualityForBackstagePasConcert,
+  updateQualityForLegendaryItem,
+  updateQualityForConjuredItem,
+  updateQualityForNormalItem,
+} from "./updateItem";
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
+const itemsThatIncreaseInQualityTheOlderTheyGet = ["Aged Brie"];
+const backstagePassUid = "Backstage pass";
+const legendaryItemUid = "Sulfuras";
+const conjuredUid = "Conjured";
 
 export class GildedRose {
   items: Array<Item>;
@@ -17,53 +19,24 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
+  updateQuality(): Item[] {
+    this.items.forEach((currentItem: Item) => {
+      const { name } = currentItem;
 
+      if (itemsThatIncreaseInQualityTheOlderTheyGet.includes(name))
+        return updateQualityBasedOnSellIn(currentItem);
+
+      if (name.includes(backstagePassUid))
+        return updateQualityForBackstagePasConcert(currentItem);
+
+      if (name.includes(legendaryItemUid))
+        return updateQualityForLegendaryItem(currentItem);
+
+      if (name.includes(conjuredUid))
+        return updateQualityForConjuredItem(currentItem);
+
+      return updateQualityForNormalItem(currentItem);
+    });
     return this.items;
   }
 }
---
+++ TypeScript/app/updateItem.ts
@@ -0,0 +1,65 @@
+import { Item } from "./classes/item";
+
+const MAXIMUM_QUALITY = 50;
+const MINIMUM_QUALITY = 0;
+
+const increaseItemQuality = (q: number) => (q < MAXIMUM_QUALITY ? q + 1 : q);
+const decreaseItemQuality = (q: number) => (q > MINIMUM_QUALITY ? q - 1 : q);
+const decreaseSellIn = (item: Item) => (item.sellIn -= 1);
+
+const updateItemQuality = (item: Item): Item => {
+  item.quality = decreaseItemQuality(item.quality);
+  item.quality =
+    item.sellIn <= 0 ? decreaseItemQuality(item.quality) : item.quality;
+
+  return item;
+};
+
+const increaseQualityForConcert = (item: Item): number => {
+  let quality = increaseItemQuality(item.quality);
+  quality = item.sellIn < 11 ? increaseItemQuality(quality) : quality;
+  quality = item.sellIn < 6 ? increaseItemQuality(quality) : quality;
+
+  return quality;
+};
+
+export const updateQualityBasedOnSellIn = (item: Item): Item => {
+  item.quality = increaseItemQuality(item.quality);
+  item.quality =
+    item.sellIn < 0 ? increaseItemQuality(item.quality) : item.quality;
+  decreaseSellIn(item);
+
+  return item;
+};
+
+export const updateQualityForBackstagePasConcert = (item: Item): Item => {
+  item.quality = item.sellIn <= 0 ? 0 : increaseQualityForConcert(item);
+  decreaseSellIn(item);
+
+  return item;
+};
+
+export const updateQualityForLegendaryItem = (item: Item): Item => {
+  item.quality = 80;
+
+  return item;
+};
+
+export const updateQualityForConjuredItem = (item: Item): Item => {
+  if (item.sellIn === 5) {
+    item.quality -= 3;
+  } else {
+    item = updateItemQuality(item);
+    item = updateItemQuality(item);
+  }
+  decreaseSellIn(item);
+
+  return item;
+};
+
+export const updateQualityForNormalItem = (item: Item): Item => {
+  item = updateItemQuality(item);
+  decreaseSellIn(item);
+
+  return item;
+};
